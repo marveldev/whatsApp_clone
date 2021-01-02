4a27f61997fdea32cd512646a792a40b73f5c2ce
Marvel Dev <marvellousalika48@gmail.com>
2021-01-02 13:37:49 +0100

    add a function that add chat to dom

--- modules/chatPage/events.js
+++ modules/chatPage/events.js
@@ -1,5 +1,18 @@
 const chatPageEventListeners = () => {
-  
+  const sendChatButton = document.querySelector('.send-button')
+  const addChatToDom = () => {
+    const chatInput = document.querySelector('.chat-input')
+    const chatInputValue = chatInput.value.trim()
+    const chatItem = `
+      <div>${chatInputValue}</div>
+    `
+    const chatContainer = document.querySelector('.chat-container')
+    chatContainer.innerHTML += chatItem
+    console.log(chatItem);
+  }
+
+  sendChatButton.addEventListener('click', addChatToDom)
+
 }
 
 export default chatPageEventListeners
