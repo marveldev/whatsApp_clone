b24ce64310386207dac34cb823b268788e1542f9
Marvel Dev <marvellousalika48@gmail.com>
2021-01-02 13:38:10 +0100

    add chat page event listener import

--- modules/defaultPage/events.js
+++ modules/defaultPage/events.js
@@ -1,13 +1,14 @@
 import chatPage from "../chatPage/chatPage.js";
+import chatPageEventListeners from "../chatPage/events.js";
 
 const defaultPageEventListeners = () => {
   const chatItems = document.querySelectorAll('.chat-item')
   for (let index = 0; index < chatItems.length; index++) {
     const chatItem = chatItems[index];
     chatItem.addEventListener('click', () => {
-      console.log('ok');
       document.querySelector('.current-page').innerHTML = chatPage()
       document.querySelector('.nav-container').style.display = 'none'
+      chatPageEventListeners()
     })
   }
 }
