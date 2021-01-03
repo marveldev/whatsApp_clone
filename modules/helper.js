2ea30f69c43c5c3f9d9ee6d51105fe721feeb8e9
Marvel Dev <marvellousalika48@gmail.com>
2021-01-03 06:52:40 +0100

    add switch current page function

--- modules/helper.js
+++ modules/helper.js
@@ -2,19 +2,39 @@ import statusPage from './statusPage/statusPage.js'
 import callsPage from './callsPage/callsPage.js'
 import defaultPage from './defaultPage/defaultPage.js'
 
-const pageEventListeners = () => {
-  const switchCurrentPage = (selector, page) => {
-    const button = document.querySelector(selector)
-    button.addEventListener('click', () => {
-      document.querySelector('.current-page').innerHTML = page
-      document.querySelector('.active').classList.remove('active')
-      button.classList.add('active')
-    })
+const switchCurrentPage = (page) => {
+  const currentPage = document.querySelector('.current-page')
+  localStorage.setItem('currentPage', page)
+  // document.querySelector('.active').classList.remove('active')
+  // button.classList.add('active')
+  
+  switch (page) {
+    case 'defaultPage':
+      currentPage.innerHTML = defaultPage()
+      break;
+    case 'statusPage':
+      currentPage.innerHTML = statusPage()
+      break;
+    case 'callsPage':
+      currentPage.innerHTML = callsPage()
+      break;
+    default:
+      currentPage.innerHTML = defaultPage()
+      break;
   }
 
-  switchCurrentPage('.chat', defaultPage())
-  switchCurrentPage('.status', statusPage())
-  switchCurrentPage('.call', callsPage())
+  // const switchCurrentPage = (selector, page) => {
+  //   const button = document.querySelector(selector)
+  //   button.addEventListener('click', () => {
+  //     document.querySelector('.current-page').innerHTML = page
+  //     document.querySelector('.active').classList.remove('active')
+  //     button.classList.add('active')
+  //   })
+  // }
+
+  // switchCurrentPage('.chat', defaultPage())
+  // switchCurrentPage('.status', statusPage())
+  // switchCurrentPage('.call', callsPage())
 }
 
-export default pageEventListeners
+export default switchCurrentPage
