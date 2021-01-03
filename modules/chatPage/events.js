29cd8c85d84a301ed3c4723b058182d320b37ba7
Marvel Dev <marvellousalika48@gmail.com>
2021-01-03 06:55:39 +0100

    add chat item display content

--- modules/chatPage/events.js
+++ modules/chatPage/events.js
@@ -4,7 +4,13 @@ const chatPageEventListeners = () => {
     const chatInput = document.querySelector('.chat-input')
     const chatInputValue = chatInput.value.trim()
     const chatItem = `
-      <div>${chatInputValue}</div>
+      <div class="person-two content">
+        <div class="arrow-right"></div>
+        <div class="text">
+          <span class="message-value">${chatInputValue}</span>
+          <sub>7:00 &#x2713;&#x2713;</sub>
+        </div>
+      </div>
     `
     const chatContainer = document.querySelector('.chat-container')
     chatContainer.innerHTML += chatItem
@@ -12,7 +18,6 @@ const chatPageEventListeners = () => {
   }
 
   sendChatButton.addEventListener('click', addChatToDom)
-
 }
 
 export default chatPageEventListeners
