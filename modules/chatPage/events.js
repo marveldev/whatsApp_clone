7d4015572f8e0f08d2fe07c60e8e92b23949642c
Marvel Dev <marvellousalika48@gmail.com>
2021-01-03 17:35:51 +0100

    add display previousPage function
    -refactor chatItem container

--- modules/chatPage/events.js
+++ modules/chatPage/events.js
@@ -1,20 +1,38 @@
+import switchCurrentPage from "../helper.js"
+
 const chatPageEventListeners = () => {
+  const arrowLeftButton = document.querySelector('#arrowLeftButton')
+  arrowLeftButton.addEventListener('click', () => {
+    switchCurrentPage('defaultPage')
+    document.querySelector('.nav-container').style.display = 'block'
+  })
+
   const sendChatButton = document.querySelector('.send-button')
   const addChatToDom = () => {
     const chatInput = document.querySelector('.chat-input')
     const chatInputValue = chatInput.value.trim()
+    const time = new Date().toTimeString()
     const chatItem = `
       <div class="person-two content">
         <div class="arrow-right"></div>
         <div class="text">
-          <span class="message-value">${chatInputValue}</span>
-          <sub>7:00 &#x2713;&#x2713;</sub>
+          <p class="message-value">${chatInputValue}</p>
+          <span class="chat-buttons">
+            <small>16:00</small>
+            <button class="delete-button"><i class="fa fa-trash"></i></button>
+            <button class="copy-button"><i class="material-icons">&#xe14d;</i></button>
+          </span>
+        </div>
+        <p class="restore-chat">Tap to restore chat in 5secs</p>
+        <div class="delete-modal">
+          <h2>Delete chat?</h2>
+          <button class="cancel button">Cancel</button>
+          <button class="delete button">Delete</button>
         </div>
       </div>
     `
     const chatContainer = document.querySelector('.chat-container')
     chatContainer.innerHTML += chatItem
-    console.log(chatItem);
   }
 
   sendChatButton.addEventListener('click', addChatToDom)
