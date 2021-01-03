44a0aef13e235116c4cfd7b8b6203fc2b26866ea
Marvel Dev <marvellousalika48@gmail.com>
2021-01-03 17:36:41 +0100

    add id name to arrow button

--- modules/chatPage/chatPage.js
+++ modules/chatPage/chatPage.js
@@ -2,7 +2,7 @@ const chatPage = () => {
   return `
     <div class="chat-page">
       <div class="chat-top-nav">
-        <button><i class="material-icons">&#xe5c4;</i></button>
+        <button id="arrowLeftButton"><i class="material-icons">&#xe5c4;</i></button>
         <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="image" alt="photo">
         <div class="chats">
