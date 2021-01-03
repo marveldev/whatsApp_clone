6d1f55a72739d38455d5b5d2b17ac5c55b9f922a
Marvel Dev <marvellousalika48@gmail.com>
2021-01-03 07:20:22 +0100

    remove switch current page import

--- modules/defaultPage/events.js
+++ modules/defaultPage/events.js
@@ -1,9 +1,7 @@
 import chatPage from "../chatPage/chatPage.js";
 import chatPageEventListeners from "../chatPage/events.js";
-import switchCurrentPage from "../helper.js";
 
 const defaultPageEventListeners = () => {
-
   const chatItems = document.querySelectorAll('.chat-item')
   for (let index = 0; index < chatItems.length; index++) {
     const chatItem = chatItems[index];
