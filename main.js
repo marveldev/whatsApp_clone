76d4c156f80d428b0fbff8157473c80f49893e8e
Marvel Dev <marvellousalika48@gmail.com>
2021-01-08 14:23:03 +0100

    remove redundant import

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 import topNav from './modules/topNav/topNav.js'
-import defaultPage from "./modules/defaultPage/defaultPage.js"
-import defaultPageEventListeners from "./modules/defaultPage/events.js"
 import topNavEventListeners from './modules/topNav/events.js'
 import switchCurrentPage from './modules/helper.js'
 import { request } from './dataStorage.js'
-import { displayItemFromDb } from './modules/chatPage/events.js'
+import { chatPageEventListeners, displayItemFromDb } from './modules/chatPage/events.js'
 
 const main = () => {
   return `
     ${topNav()}
     <div class="current-page">
-      ${defaultPage()}
     </div>
   `
 }
 
 request.onsuccess = async () => {
   document.querySelector('.main').innerHTML = main()
+
   const currentPage = localStorage.getItem('currentPage')
   switchCurrentPage(currentPage || 'defaultPage')
 
   if (currentPage === 'chatPage') {
     await switchCurrentPage(currentPage)
     displayItemFromDb()
+    // chatPageEventListeners()
   }
 
-  defaultPageEventListeners()
   topNavEventListeners()
 }
