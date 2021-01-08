9b39e20de6d2a2ab3e06289464d2787133035f37
Marvel Dev <marvellousalika48@gmail.com>
2021-01-08 06:50:02 +0100

    rename div class attribute to id

--- modules/chatPage/chatPage.js
+++ modules/chatPage/chatPage.js
@@ -48,7 +48,7 @@ const chatPage = () => {
         <button id="arrowLeftButton"><i class="material-icons">&#xe5c4;</i></button>
         <img src="https://images.pexels.com/photos/4724071/pexels-photo-4724071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="image" alt="photo">
-        <div class="chats">
+        <div id="chats">
           <strong>Jack Williams</strong>
           <p>online</p>
         </div>
