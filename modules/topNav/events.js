36c9ff05ab3e10e35eac68a2648d9e290751a2d2
Marvel Dev <marvellousalika48@gmail.com>
2021-01-11 13:43:37 +0100

    add topnav dropdown event

--- modules/topNav/events.js
+++ modules/topNav/events.js
@@ -1,6 +1,12 @@
 import switchCurrentPage from "../helper.js"
 
 const topNavEventListeners = () => {
+  const topNavDropdownButton = document.querySelector('.top-nav-dropdown')
+  topNavDropdownButton.addEventListener('click', () => {
+    document.querySelector('#topNavOverlay').style.display = 'block'
+    document.querySelector('#topNavDropdown').style.display = 'block'
+  })
+
   const addSwitchPageEvent = (button, page) => {
     button.addEventListener('click', () => {
       switchCurrentPage(page)
