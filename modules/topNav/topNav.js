eebef3aa44bb5bb135de440578819999a89dfaeb
Marvel Dev <marvellousalika48@gmail.com>
2021-01-11 16:49:19 +0100

    refactor top nav html

--- modules/topNav/topNav.js
+++ modules/topNav/topNav.js
@@ -1,25 +1,28 @@
 const topNav = () => {
   return `
     <div class="top-nav">
-      <div class="top-header">
-        <span>WhatsApp</span>
-        <div>
-          <button class="search-button"><i class="fa fa-search"></i></button>
-          <button class="top-nav-dropdown"><i class="material-icons">&#xe5d4;</i></button>
+      <div id="topNavOverlay"></div>
+      <div class="top-nav-content">
+        <div class="top-header">
+          <span>WhatsApp</span>
+          <div>
+            <button class="search-button"><i class="fa fa-search"></i></button>
+            <button class="top-nav-dropdown"><i class="material-icons">&#xe5d4;</i></button>
+          </div>
         </div>
-      </div>
-      <nav class="nav-button">
-        <button type="button"><i class="fa fa-camera"></i></button>
-        <button type="button" class="default active">CHATS</button>
-        <button type="button" class="status">STATUS</button>
-        <button type="button" class="call">CALLS</button>
-      </nav>
-      <div id="topNavDropdown" class="dropdown-modal">
-        <button>New group</button>
-        <button>New broadcast</button>
-        <button>WhatsApp Web</button>
-        <button>Starred messages</button>
-        <button id="settingsButton">Settings</button>
+        <nav>
+          <button type="button"><i class="fa fa-camera"></i></button>
+          <button type="button" class="default active">CHATS</button>
+          <button type="button" class="status">STATUS</button>
+          <button type="button" class="call">CALLS</button>
+        </nav>
+        <div id="topNavDropdown" class="dropdown-modal">
+          <button>New group</button>
+          <button>New broadcast</button>
+          <button>WhatsApp Web</button>
+          <button>Starred messages</button>
+          <button class="settings">Settings</button>
+        </div>
       </div>
     </div>
   `
