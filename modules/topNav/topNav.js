d711af178dcf6fa65fe5bf5469ae3b2e545fca09
Marvel Dev <marvellousalika48@gmail.com>
2021-01-11 13:44:04 +0100

    add topnav dropdown html

--- modules/topNav/topNav.js
+++ modules/topNav/topNav.js
@@ -1,19 +1,26 @@
 const topNav = () => {
   return `
-    <div class="nav-container">
+    <div class="top-nav">
       <div class="top-header">
         <span>WhatsApp</span>
         <div>
           <button class="search-button"><i class="fa fa-search"></i></button>
-          <button class="more-button"><i class="material-icons">&#xe5d4;</i></button>
+          <button class="top-nav-dropdown"><i class="material-icons">&#xe5d4;</i></button>
         </div>
       </div>
-      <nav class="chat-nav">
-        <button class="button"><i class="fa fa-camera"></i></button>
-        <button class="default button active">CHATS</button>
-        <button class="status button">STATUS</button>
-        <button class="call button">CALLS</button>
+      <nav class="nav-button">
+        <button type="button"><i class="fa fa-camera"></i></button>
+        <button type="button" class="default active">CHATS</button>
+        <button type="button" class="status">STATUS</button>
+        <button type="button" class="call">CALLS</button>
       </nav>
+      <div id="topNavDropdown" class="dropdown-modal">
+        <button>New group</button>
+        <button>New broadcast</button>
+        <button>WhatsApp Web</button>
+        <button>Starred messages</button>
+        <button id="settingsButton">Settings</button>
+      </div>
     </div>
   `
 }
