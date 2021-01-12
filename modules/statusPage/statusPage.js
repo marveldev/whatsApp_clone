818e224e193c899992ed8bf8f57e685e85f3bb51
Marvel Dev <marvellousalika48@gmail.com>
2021-01-12 17:01:27 +0100

    remove redundant code

--- modules/statusPage/statusPage.js
+++ modules/statusPage/statusPage.js
@@ -13,28 +13,13 @@ const statusPage = () => {
       <div>
         <button id="viewStatusButton" class="chat-list">
           <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-            class="image" alt="photo">
+            id="statusProfilePhoto" class="image" alt="photo">
           <div>
             <strong>My status</strong>
-            <p>Tap to add status update</p>
+            <p id="statusInfo">Tap to add status update</p>
           </div>
         </button>
-        <div class="status-photo-container">
-          <div>
-            <div class="progress-bar"><div class="bar"></div></div>
-            <div class="status-profile">
-              <button type="button" id="returnButton"><i class="material-icons">&#xe5c4;</i></button>
-              <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-                class="image" alt="photo">
-              <div>
-                <strong>Jack Williams</strong>
-                <small>5 mins ago</small>
-              </div>
-            </div>
-            <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-              class="status-photo" alt="photo">
-          </div>
-        </div>
+        <div class="status-photo-container"></div>
       </div>
     </div>
   `
