8f994b5ae3edb782a1bd9324fba5712d8b323e21
Marvel Dev <marvellousalika48@gmail.com>
2021-01-12 17:00:56 +0100

    initial commit

--- modules/statusPage/events.js
+++ modules/statusPage/events.js
@@ -1,12 +1,40 @@
-const statusPageEventListener = () => {
-  document.querySelector('#addStatusButton').addEventListener('click', () => {
-    console.log('ok');
-  })
+import { addEntryToDb } from "../../dataStorage.js"
 
+const statusPageEventListener = () => {
+  const addStatusPhoto = document.querySelector('#addStatus')
+  addStatusPhoto.addEventListener('change', () => {
+    const photoReader = new FileReader()
+    photoReader.readAsDataURL(addStatusPhoto.files[0])
+    photoReader.addEventListener('load', () => {
+      const statusPhoto =  `
+        <div class="status-photo-item">
+          <div class="progress-bar"><div class="bar"></div></div>
+          <div class="status-profile">
+            <button type="button" class="close-button"><i class="material-icons">&#xe5c4;</i></button>
+            <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+              class="image" alt="photo">
+            <div>
+              <strong>Jack Williams</strong>
+              <small>5 mins ago</small>
+            </div>
+          </div>
+          <div>
+            <img src=${photoReader.result} class="status-photo" alt="photo">
+          </div>
+        </div>
+      `
+      document.querySelector('.status-photo-container').innerHTML += statusPhoto
+      console.log(document.querySelector('.status-photo-container'));
+      document.querySelector('#statusProfilePhoto').src = photoReader.result
+      document.querySelector('#statusProfilePhoto').classList.add('circle')
+      document.querySelector('#statusInfo').innerText = `Tap to view status update`
+      // addEntryToDb('statusPhoto', photoReader.result)
+    })
+  })
 
   document.querySelector('#viewStatusButton').addEventListener('click', () => {
     document.querySelector('.top-nav').style.display = 'none'
-    document.querySelector('.status-photo-container').style.display = 'block'
+    document.querySelector('.status-photo-container').style.display = 'grid'
 
     const bar = document.querySelector('.bar')
     let width = 1
@@ -21,10 +49,10 @@ const statusPageEventListener = () => {
     const interval = setInterval(progress, 10)
   })
 
-  document.querySelector('#returnButton').addEventListener('click', () => {
-    document.querySelector('.top-nav').style.display = 'block'
-    document.querySelector('.status-photo-container').style.display = 'none'
-  })
+  // document.querySelector('.close-button').addEventListener('click', () => {
+  //   document.querySelector('.top-nav').style.display = 'block'
+  //   document.querySelector('.status-photo-container').style.display = 'none'
+  // })
 }
 
 export default statusPageEventListener
