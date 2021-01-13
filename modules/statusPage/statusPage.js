3d2f56ed692abf96a8927f452986deadc23c2229
Marvel Dev <marvellousalika48@gmail.com>
2021-01-13 06:26:37 +0100

    add html for status photo content

--- modules/statusPage/statusPage.js
+++ modules/statusPage/statusPage.js
@@ -19,7 +19,59 @@ const statusPage = () => {
             <p id="statusInfo">Tap to add status update</p>
           </div>
         </button>
-        <div class="status-photo-container"></div>
+        <div class="status-photo-container">
+          <div id="statusPhotoContent">
+            <div class="status-photo-item current">
+              <div class="progress-bar"><div class="bar"></div></div>
+              <div class="status-profile">
+                <button type="button" class="close-button"><i class="material-icons">&#xe5c4;</i></button>
+                <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+                  class="image" alt="photo">
+                <div>
+                  <strong>Jack Williams</strong>
+                  <small>5 mins ago</small>
+                </div>
+              </div>
+              <div>
+                <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="status-photo" alt="photo">
+              </div>
+            </div>
+            <div class="status-photo-item">
+              <div class="progress-bar"><div class="bar"></div></div>
+              <div class="status-profile">
+                <button type="button" class="close-button"><i class="material-icons">&#xe5c4;</i></button>
+                <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+                  class="image" alt="photo">
+                <div>
+                  <strong>Jack Williams</strong>
+                  <small>5 mins ago</small>
+                </div>
+              </div>
+              <div>
+                <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="status-photo" alt="photo">
+              </div>
+            </div>
+            <div class="status-photo-item">
+              <div class="progress-bar"><div class="bar"></div></div>
+              <div class="status-profile">
+                <button type="button" class="close-button"><i class="material-icons">&#xe5c4;</i></button>
+                <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+                  class="image" alt="photo">
+                <div>
+                  <strong>Jack Williams</strong>
+                  <small>5 mins ago</small>
+                </div>
+              </div>
+              <div>
+                <img src="https://images.pexels.com/photos/6341409/pexels-photo-6341409.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="status-photo" alt="photo">
+              </div>
+            </div>
+          </div>
+          <div>
+            <button id="previousButton">previous</button>
+            <button id="nextButton">next</button>
+          </div>
+        </div>
       </div>
     </div>
   `
