e0cb5b952126013fea58f9886e1f1101fa4f2dcb
Marvel Dev <marvellousalika48@gmail.com>
2021-01-13 06:25:49 +0100

    add display for status photo content

--- modules/statusPage/events.js
+++ modules/statusPage/events.js
@@ -10,7 +10,7 @@ const statusPageEventListener = () => {
         <div class="status-photo-item">
           <div class="progress-bar"><div class="bar"></div></div>
           <div class="status-profile">
-            <button type="button" class="close-button"><i class="material-icons">&#xe5c4;</i></button>
+            <button type="button" class="close-button"><i class="material-icons">&#xe5c4</i></button>
             <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
               class="image" alt="photo">
             <div>
@@ -23,30 +23,54 @@ const statusPageEventListener = () => {
           </div>
         </div>
       `
-      document.querySelector('.status-photo-container').innerHTML += statusPhoto
-      console.log(document.querySelector('.status-photo-container'));
-      document.querySelector('#statusProfilePhoto').src = photoReader.result
-      document.querySelector('#statusProfilePhoto').classList.add('circle')
-      document.querySelector('#statusInfo').innerText = `Tap to view status update`
+      document.querySelector('#statusPhotoContent').innerHTML += statusPhoto
+      // document.querySelector('#statusProfilePhoto').src = photoReader.result
+      // document.querySelector('#statusProfilePhoto').classList.add('circle')
+      // document.querySelector('#statusInfo').innerText = `Tap to view status update`
       // addEntryToDb('statusPhoto', photoReader.result)
     })
   })
 
   document.querySelector('#viewStatusButton').addEventListener('click', () => {
     document.querySelector('.top-nav').style.display = 'none'
-    document.querySelector('.status-photo-container').style.display = 'grid'
+    document.querySelector('.status-photo-container').style.display = 'block'
 
-    const bar = document.querySelector('.bar')
-    let width = 1
-    const progress = () => {
-      if (width >= 100) {
-        clearInterval(interval)
-      } else {
-        width++;
-        bar.style.width = width + '%';
-      }
+    // const bar = document.querySelector('.bar')
+    // let width = 1
+    // const progress = () => {
+    //   if (width >= 100) {
+    //     // clearInterval(interval)
+    //   } else {
+    //     width++
+    //     bar.style.width = width + '%'
+    //   }
+    // }
+    // const interval = setInterval(progress, 10)
+  })
+
+  
+  document.querySelector('#nextButton').addEventListener('click', () => {
+    const slides = document.querySelectorAll('.status-photo-item')
+    const currentSlide = document.querySelector('.current')
+    currentSlide.classList.remove('current')
+  
+    if (currentSlide.nextElementSibling) {
+      currentSlide.nextElementSibling.classList.add('current')
+    } else {
+      slides[0].classList.add('current')
     }
-    const interval = setInterval(progress, 10)
+  })
+
+  document.querySelector('#previousButton').addEventListener('click', () => {
+    const slides = document.querySelectorAll('.status-photo-item')
+    const currentSlide = document.querySelector('.current')
+    currentSlide.classList.remove('current')
+
+    if (currentSlide.previousElementSibling) {
+      currentSlide.previousElementSibling.classList.add('current')
+    } else {
+      slides[slides.length - 1].classList.add('current')
+    }
   })
 
   // document.querySelector('.close-button').addEventListener('click', () => {
