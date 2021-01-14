15e192e4f0b9813e0e6eb69d07cd4190eea380d9
Marvel Dev <marvellousalika48@gmail.com>
2021-01-14 08:40:56 +0100

    remove dropdown display

--- modules/settingsPage/events.js
+++ modules/settingsPage/events.js
@@ -4,6 +4,8 @@ const settingsPageEventListener = () => {
   document.querySelector('.return-Button').addEventListener('click', () => {
     switchCurrentPage('defaultPage')
     document.querySelector('.top-nav').style.display = 'block'
+    document.querySelector('#topNavDropdown').style.display = 'none'
+    document.querySelector('#topNavOverlay').style.display = 'none'
   })
 }
 
