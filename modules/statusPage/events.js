9887f26fd45c39862605c742588607c313983612
Marvel Dev <marvellousalika48@gmail.com>
2021-01-14 12:34:16 +0100

    add photo progress function

--- modules/statusPage/events.js
+++ modules/statusPage/events.js
@@ -21,16 +21,45 @@ const statusPageEventListener = () => {
 
   addStatusPhoto(document.querySelector('#addStatus'))
 
+  let interval;
+
+  const progress = () => {
+    let bar = document.querySelector('.bar')
+    let width = 1
+    interval = setInterval(() => {
+      if (width >= 100) {
+        const slides = document.querySelectorAll('.status-photo')
+        const currentSlide = document.querySelector('.current')
+        currentSlide.classList.remove('current')
+        if (currentSlide.nextElementSibling) {
+          currentSlide.nextElementSibling.classList.add('current')
+          width = 1
+        } else {
+          slides[0].classList.add('current')
+          document.querySelector('.top-nav').style.display = 'block'
+          document.querySelector('.status-photo-container').style.display = 'none'
+          clearInterval(interval)
+        }
+      } else {
+        width++
+        bar.style.width = width + '%'
+      }
+    }, 30)
+  }
+
   document.querySelector('.view-status').addEventListener('click', () => {
     document.querySelector('.top-nav').style.display = 'none'
     document.querySelector('.status-photo-container').style.display = 'block'
     document.querySelector('#statusPhotoContent').firstElementChild.classList.add('current')
+    progress()
   })
 
   document.querySelector('#nextButton').addEventListener('click', () => {
+    clearInterval(interval)
     const slides = document.querySelectorAll('.status-photo')
     const currentSlide = document.querySelector('.current')
     currentSlide.classList.remove('current')
+    document.querySelector('.bar').style.width = 100 + '%'
     if (currentSlide.nextElementSibling) {
       currentSlide.nextElementSibling.classList.add('current')
     } else {
@@ -39,9 +68,11 @@ const statusPageEventListener = () => {
   })
 
   document.querySelector('#previousButton').addEventListener('click', () => {
+    clearInterval(interval)
     const slides = document.querySelectorAll('.status-photo')
     const currentSlide = document.querySelector('.current')
     currentSlide.classList.remove('current')
+    document.querySelector('.bar').style.width = 100 + '%'
     if (currentSlide.previousElementSibling) {
       currentSlide.previousElementSibling.classList.add('current')
     } else {
@@ -49,8 +80,18 @@ const statusPageEventListener = () => {
     }
   })
 
+  const statusPhotos = document.querySelectorAll('.status-photo')
+  for (let index = 0; index < statusPhotos.length; index++) {
+    const statusPhoto = statusPhotos[index]
+    statusPhoto.addEventListener('mousedown', () => {
+      alert()
+      console.log('ok');
+    })
+  }
+
   document.querySelector('.close-status-button').addEventListener('click', () => {
     document.querySelector('.top-nav').style.display = 'block'
+    document.querySelector('.current').classList.remove('current')
     document.querySelector('.status-photo-container').style.display = 'none'
   })
 }
