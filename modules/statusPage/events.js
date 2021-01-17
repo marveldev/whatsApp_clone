53e4ea13a294f994087584867814fb8d484dfb17
Marvel Dev <marvellousalika48@gmail.com>
2021-01-17 17:31:06 +0100

    declare statusItem event function before usage

--- modules/statusPage/events.js
+++ modules/statusPage/events.js
@@ -1,4 +1,50 @@
-import { addEntryToDb } from "../../dataStorage.js"
+import { addEntryToDb, deleteEntry } from "../../dataStorage.js"
+
+const statusItemEvent = () => {
+  let singleItemId;
+  const itemDropdownIcons =  document.querySelectorAll('.item-dropdown-icon')
+  for (let index = 0; index < itemDropdownIcons.length; index++) {
+    const itemDropdownIcon = itemDropdownIcons[index]
+    itemDropdownIcon.addEventListener('click', () => {
+      document.querySelector('#statusItemDropdown').style.display = 'block'
+      document.querySelector('#statusOverlay').style.display = 'block'
+      singleItemId = itemDropdownIcon.id
+    })
+  }
+
+  document.querySelector('.previous-button').addEventListener('click', () => {
+    document.querySelector('.top-nav').style.display = 'block'
+    document.querySelector('#statusMainContent').style.display = 'block'
+    document.querySelector('.status-entry-options').style.display = 'none'
+  })
+
+  document.querySelector('#statusOverlay').addEventListener('click', () => {
+    document.querySelector('#statusItemDropdown').style.display = 'none'
+    document.querySelector('.status-delete-modal').style.display = 'none'
+    document.querySelector('#statusOverlay').style.display = 'none'
+  })
+
+  document.querySelector('.delete-modal-button').addEventListener('click', () => {
+    document.querySelector('#statusItemDropdown').style.display = 'none'
+    document.querySelector('.status-delete-modal').style.display = 'block'
+  })
+
+  document.querySelector('.delete-item-button').addEventListener('click', () => {
+    const element = document.querySelector(`#${singleItemId}`)
+    const singleStatusEntry = element.parentElement
+    document.querySelector('.status-item-container').removeChild(singleStatusEntry)
+    document.querySelector('#statusOverlay').style.display = 'none'
+    document.querySelector('.status-delete-modal').style.display = 'none'
+    document.querySelector('#statusItemDropdown').style.display = 'none'
+
+    deleteEntry('statusData', singleItemId)
+  })
+
+  document.querySelector('.close-modal-button').addEventListener('click', () => {
+    document.querySelector('#statusOverlay').style.display = 'none'
+    document.querySelector('.status-delete-modal').style.display = 'none'
+  })
+}
 
 const statusPageEventListener = () => {
   const statusFilePicker = document.querySelector('#addStatus')
@@ -11,18 +57,18 @@ const statusPageEventListener = () => {
         <img src="${photoReader.result}" class="status-photo" alt="photo">
       `
 
-      const statusItemPreview = `
-        <button id="${itemId}" class="status-item-preview">
+      const singleStatusEntry = `
+        <button class="status-item-preview">
           <img src="${photoReader.result}" class="image" alt="photo">
           <div class="status-info">
             <strong>18 views</strong>
             <p>Today 06:03</p>
           </div>
-          <span class="item-dropdown-icon"><i class="material-icons">&#xe5d4;</i></span>
+          <span id="${itemId}" class="item-dropdown-icon"><i class="material-icons">&#xe5d4;</i></span>
         </button>
       `
       document.querySelector('#statusItemContent').innerHTML += statusData
-      document.querySelector('.status-item-container').innerHTML += statusItemPreview
+      document.querySelector('.status-item-container').innerHTML += singleStatusEntry
       document.querySelector('#statusPreview').src = photoReader.result
       document.querySelector('.view-status').style.display = 'flex'
       document.querySelector('.add-status').style.display = 'none'
@@ -103,47 +149,11 @@ const statusPageEventListener = () => {
     clearInterval(interval)
   })
 
-  const displayElement = (value1, value2) => {
-    document.querySelector('.top-nav').style.display = value1
-    document.querySelector('#statusMainContent').style.display = value1
-    document.querySelector('.status-entry-options').style.display = value2
-  }
-
-  document.querySelector('.previous-button').addEventListener('click', () => {
-    displayElement('block', 'none')
-  })
-
   document.querySelector('#entryOptionsButton').addEventListener('click', () => {
-    displayElement('none', 'block')
-  })
-
-  const itemDropdownIcons =  document.querySelectorAll('.item-dropdown-icon')
-  for (let index = 0; index < itemDropdownIcons.length; index++) {
-    const itemDropdownIcon = itemDropdownIcons[index]
-    itemDropdownIcon.addEventListener('click', () => {
-      document.querySelector('#statusItemDropdown').style.display = 'block'
-      document.querySelector('#statusOverlay').style.display = 'block'
-    })
-  }
-
-  document.querySelector('#statusOverlay').addEventListener('click', () => {
-    document.querySelector('#statusItemDropdown').style.display = 'none'
-    document.querySelector('#statusOverlay').style.display = 'none'
-  })
-
-  document.querySelector('.delete-modal-button').addEventListener('click', () => {
-    document.querySelector('#statusItemDropdown').style.display = 'none'
-    document.querySelector('.status-delete-modal').style.display = 'block'
-  })
-
-  document.querySelector('.delete-item-button').addEventListener('click', () => {
-    console.log('ok');
-    console.log(document.querySelector('.status-item-container'));
-  })
-
-  document.querySelector('.close-modal-button').addEventListener('click', () => {
-    document.querySelector('#statusOverlay').style.display = 'none'
-    document.querySelector('.status-delete-modal').style.display = 'none'
+    document.querySelector('.top-nav').style.display = 'none'
+    document.querySelector('#statusMainContent').style.display = 'none'
+    document.querySelector('.status-entry-options').style.display = 'block'
+    statusItemEvent()
   })
 }
 
