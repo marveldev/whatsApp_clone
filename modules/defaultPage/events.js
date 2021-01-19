e30086b3320ff5fd12dc417c250a07e58e06f5a4
Marvel Dev <marvellousalika48@gmail.com>
2021-01-19 16:51:54 +0100

    add person profile photo events
    - rename chat lists to person profile

--- modules/defaultPage/events.js
+++ modules/defaultPage/events.js
@@ -1,11 +1,19 @@
 import switchCurrentPage from "../helper.js"
 
 const defaultPageEventListeners = () => {
-  const chatLists = document.querySelectorAll('.chat-list')
-  for (let index = 0; index < chatLists.length; index++) {
-    const chatList = chatLists[index];
-    chatList.addEventListener('click', () => {
+  const personProfiles = document.querySelectorAll('.person-profile')
+  for (let index = 0; index < personProfiles.length; index++) {
+    const personProfile = personProfiles[index];
+    personProfile.addEventListener('click', () => {
       switchCurrentPage('chatPage')
+    })
+  }
+
+  const personProfilePhotos = document.querySelectorAll('.person-photo')
+  for (let index = 0; index < personProfilePhotos.length; index++) {
+    const personProfilePhoto = personProfilePhotos[index];
+    personProfilePhoto.addEventListener('click', () => {
+      document.querySelector('#defaultPageOverlay').style.display = 'block'
     })
   }
 }
