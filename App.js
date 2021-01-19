5d5c0d37b1314654c1faa724d047d9964a54557c
Marvel Dev <marvellousalika48@gmail.com>
2021-01-19 16:48:41 +0100

    add display of entry from pusher

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import TopNav from './modules/topNav/TopNav.js'
 import topNavEventListeners from './modules/topNav/events.js'
 import switchCurrentPage from './modules/helper.js'
-import { request } from './dataStorage.js'
+import { addEntryToDb, request } from './dataStorage.js'
+import addStatusFile from './modules/statusPage/events.js'
 
 const App = () => {
   return `
+    <div id="defaultPageOverlay"></div>
     ${TopNav()}
     <div class="current-page">
     </div>
@@ -13,6 +15,7 @@ const App = () => {
 
 request.onsuccess = async () => {
   document.querySelector('.root').innerHTML = App()
+  const recentEntryDiv =  document.querySelector('.recent-entry')
   const currentPage = localStorage.getItem('currentPage')
 
   if (currentPage === 'chatPage') {
@@ -22,4 +25,30 @@ request.onsuccess = async () => {
   }
 
   topNavEventListeners()
+
+  const pusher = new Pusher('28732b89eff34d2e9cb2', {
+    cluster: 'mt1'
+  })
+
+  if (currentPage === 'statusPage') {
+    const channel = pusher.subscribe('status')
+    channel.bind('update-status', data => {
+      const { text, backgroundColor } = data
+      addStatusFile(text, null, backgroundColor)
+      recentEntryDiv.innerText = text
+      recentEntryDiv.style.backgroundColor = backgroundColor
+    })
+  } else {
+    const itemId = 'id' + Date.parse(new Date()).toString()
+    const channel = pusher.subscribe('status')
+    channel.bind('update-status', data => {
+      const { text, backgroundColor } = data
+      const statusObject = {
+        itemId: itemId,
+        textValue: text,
+        entryBackgroundColor: backgroundColor
+      }
+      addEntryToDb('statusData', statusObject)
+    })
+  }
 }
