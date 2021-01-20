727d9db42455da9b88544f77440ff4d4677f6e8d
Marvel Dev <marvellousalika48@gmail.com>
2021-01-20 08:33:15 +0100

    add default page overlay display
    - remove redundant code

--- modules/topNav/events.js
+++ modules/topNav/events.js
@@ -3,13 +3,14 @@ import switchCurrentPage from "../helper.js"
 const topNavEventListeners = () => {
   const topNavDropdownButton = document.querySelector('.top-nav-dropdown')
   topNavDropdownButton.addEventListener('click', () => {
-    document.querySelector('#topNavOverlay').style.display = 'block'
     document.querySelector('#topNavDropdown').style.display = 'block'
+    document.querySelector('#defaultPageOverlay').style.display = 'block'
   })
 
   const addSwitchPageEvent = (selector, page) => {
     selector.addEventListener('click', () => {
       switchCurrentPage(page)
+      document.querySelector('#defaultPageOverlay').style.display = 'none'
     })
   }
 
@@ -17,11 +18,6 @@ const topNavEventListeners = () => {
   addSwitchPageEvent(document.querySelector('.status'), 'statusPage')
   addSwitchPageEvent(document.querySelector('.call'), 'callsPage')
   addSwitchPageEvent(document.querySelector('.settings'), 'settingsPage')
-
-  // document.querySelector('#topNavOverlay').addEventListener('click', () => {
-  //   document.querySelector('#topNavDropdown').style.display = 'none'
-  //   document.querySelector('#topNavOverlay').style.display = 'none'
-  // })
 }
 
 export default topNavEventListeners
