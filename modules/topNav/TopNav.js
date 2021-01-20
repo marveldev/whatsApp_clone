0582f4a52959941460585373dae862067036fc2b
Marvel Dev <marvellousalika48@gmail.com>
2021-01-20 08:34:03 +0100

    refactor top nav html

--- modules/topNav/TopNav.js
+++ modules/topNav/TopNav.js
@@ -1,7 +1,7 @@
 const TopNav = () => {
   return `
     <div class="top-nav">
-      <div class="top-nav-content">
+      <div id="topNav">
         <div class="top-header">
           <span>WhatsApp</span>
           <div>
@@ -10,18 +10,18 @@ const TopNav = () => {
           </div>
         </div>
         <nav>
-          <button type="button"><i class="fa fa-camera"></i></button>
-          <button type="button" class="default active">CHATS</button>
-          <button type="button" class="status">STATUS</button>
-          <button type="button" class="call">CALLS</button>
+          <button type="button"class="page"><i class="fa fa-camera"></i></button>
+          <button type="button" class="default active page">CHATS</button>
+          <button type="button" class="status page">STATUS</button>
+          <button type="button" class="call page">CALLS</button>
         </nav>
-        <div id="topNavDropdown" class="dropdown-modal">
-          <button>New group</button>
-          <button>New broadcast</button>
-          <button>WhatsApp Web</button>
-          <button>Starred messages</button>
-          <button class="settings">Settings</button>
-        </div>
+      </div>
+      <div id="topNavDropdown" class="dropdown-modal">
+        <button>New group</button>
+        <button>New broadcast</button>
+        <button>WhatsApp Web</button>
+        <button>Starred messages</button>
+        <button class="settings">Settings</button>
       </div>
     </div>
   `
