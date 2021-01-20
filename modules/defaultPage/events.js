ddce2ea1cacb6b28b5cef36e6d7bec8c939d2d23
Marvel Dev <marvellousalika48@gmail.com>
2021-01-20 08:31:20 +0100

    add default page overlay event listener

--- modules/defaultPage/events.js
+++ modules/defaultPage/events.js
@@ -3,7 +3,7 @@ import switchCurrentPage from "../helper.js"
 const defaultPageEventListeners = () => {
   const personProfiles = document.querySelectorAll('.person-profile')
   for (let index = 0; index < personProfiles.length; index++) {
-    const personProfile = personProfiles[index];
+    const personProfile = personProfiles[index]
     personProfile.addEventListener('click', () => {
       switchCurrentPage('chatPage')
     })
@@ -11,11 +11,17 @@ const defaultPageEventListeners = () => {
 
   const personProfilePhotos = document.querySelectorAll('.person-photo')
   for (let index = 0; index < personProfilePhotos.length; index++) {
-    const personProfilePhoto = personProfilePhotos[index];
+    const personProfilePhoto = personProfilePhotos[index]
     personProfilePhoto.addEventListener('click', () => {
       document.querySelector('#defaultPageOverlay').style.display = 'block'
+      document.querySelector('.person-photo-modal').style.display = 'block'
     })
   }
+
+  document.querySelector('#defaultPageOverlay').addEventListener('click', () => {
+    document.querySelector('.person-photo-modal').style.display = 'none'
+    document.querySelector('#defaultPageOverlay').style.display = 'none'
+  })
 }
 
 export default defaultPageEventListeners
