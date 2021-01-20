038b53797a1bea2b333e15c08498f08012919f28
Marvel Dev <marvellousalika48@gmail.com>
2021-01-20 08:30:54 +0100

    add person photo modal html

--- modules/defaultPage/DefaultPage.js
+++ modules/defaultPage/DefaultPage.js
@@ -1,116 +1,186 @@
 const DefaultPage = () => {
   return `
     <div class="default-page">
-      <button class="chat-persons">
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
+        </button>
+      </div>
+      <div class="chat-persons">
         <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
           class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
+        <div class="person-photo-modal">
+          <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            class="modal-photo" alt="photo">
+          <div class="container">
+            <button><i class="material-icons">&#xe0b7;</i></button>
+            <button><i class="fa fa-phone"></i></button>
+            <button><i class="fa fa-video-camera"></i></button>
+            <button><i class="material-icons">&#xe88f;</i></button>
+          </div>
+        </div>
+        <button class="person-profile status-info">
           <strong>Jack Williams</strong>
           <div>
             <p>Hello</p>
           </div>
-        </div>
-      </button>
-      <button class="chat-persons">
-        <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-          class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
-          <strong>Jack Williams</strong>
-          <div>
-            <p>Hello</p>
-          </div>
-        </div>
-      </button>
-      <button class="chat-persons">
-        <img src="https://images.pexels.com/photos/4119310/pexels-photo-4119310.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
-          class="person-photo image" alt="photo">
-        <div class="person-profile status-info">
-          <strong>Jack Williams</strong>
-          <div>
-            <p>Hello</p>
-          </div>
-        </div>
-      </button>
+        </button>
+      </div>
       <div>
         <button class="contact icon"><i class="material-icons">&#xe0b7;</i></button>
       </div>
