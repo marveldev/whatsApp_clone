77b01643c5c18fe733f87bacf3b970fb7a7d9534
Marvel Dev <marvellousalika48@gmail.com>
2021-01-22 19:23:35 +0100

    add theme function

--- modules/settingsPage/events.js
+++ modules/settingsPage/events.js
@@ -14,6 +14,16 @@ const settingsPageEventListener = () => {
 }
 
 const toggleTheme = () => {
+  const radioButtons = document.querySelectorAll('.radio')
+  const theme = localStorage.getItem('theme')
+  let checkBox
+
+  if (theme) {
+    const themeName = document.querySelector('#themeName')
+    themeName.innerText = theme.charAt(0).toUpperCase() + theme.slice(1)
+    document.querySelector(`input[value=${theme}]`).setAttribute('checked', '')
+  }
+
   document.querySelector('.back-button').addEventListener('click', () => {
     switchCurrentPage('settingsPage')
   })
@@ -28,8 +38,20 @@ const toggleTheme = () => {
     document.querySelector('.theme-container').style.display = 'none'
   })
 
+  for (let index = 0; index <  radioButtons.length; index++) {
+    const  radioButton =  radioButtons[index]
+    radioButton.addEventListener('change', () => {
+      checkBox = radioButton.firstElementChild
+    })
+  }
+
   document.querySelector('.confirm-theme-button').addEventListener('click', () => {
-    localStorage.setItem('theme', 'dark')
+    const checkBoxValue = checkBox.value
+    localStorage.setItem('theme', checkBoxValue)
+    document.body.className = checkBoxValue
+    const themeName = document.querySelector('#themeName')
+    themeName.innerText = checkBoxValue.charAt(0).toUpperCase() + checkBoxValue.slice(1)
+    checkBox.setAttribute('checked', '')
     document.querySelector('#defaultPageOverlay').style.display = 'none'
     document.querySelector('.theme-container').style.display = 'none'
   })
