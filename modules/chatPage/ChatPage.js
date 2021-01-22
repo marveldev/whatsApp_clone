c05d0f04fd68186e5f79339ecb237d36919ec28e
Marvel Dev <marvellousalika48@gmail.com>
2021-01-22 14:38:32 +0100

    add function to display chat background

--- modules/chatPage/ChatPage.js
+++ modules/chatPage/ChatPage.js
@@ -46,8 +46,19 @@ const ChatPage = async () => {
     `
   })
 
+  const chatBackground = () => {
+    let backgroundUrl
+    const theme = localStorage.getItem('theme')
+    if (theme === 'light') {
+      backgroundUrl = chatPageBackground[0] || `https://cloud.githubusercontent.com/assets/398893/15136779/4e765036-1639-11e6-9201-67e728e86f39.jpg`
+    } else {
+      backgroundUrl = chatPageBackground[0] || 'https://i.ibb.co/q9mygMq/background.jpg'
+    }
+    return backgroundUrl
+  }
+
   return `
-    <div class="chat-page" style="background-image: url(${chatPageBackground[0] || `https://cloud.githubusercontent.com/assets/398893/15136779/4e765036-1639-11e6-9201-67e728e86f39.jpg`});">
+    <div class="chat-page" style="background-image: url(${chatBackground()});">
       <div id="chatOverlay" class="overlay"></div>
       <div class="chat-top-nav">
         <button type="button" id="arrowLeftButton">
